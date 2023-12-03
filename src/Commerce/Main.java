3def6c9d2f3cbbf2742e72cc5a173c3f9f01d13e
Buttthien <bangbangxombun1995@gmail.com>
2023-12-03 18:06:52 +0700

    Commerce

---
+++ src/Commerce/Main.java
@@ -0,0 +1,39 @@
+
+//import Commerce.AccountSignIn;
+
+package Commerce;
+
+//import java.awt.Toolkit;
+//import java.awt.event.WindowEvent;
+
+
+//import Commerce.AccountSignIn;
+//import Commerce.AccountSignUp;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    /*
+    public void close(){
+        WindowEvent closeW = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+        Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(closeW);
+    }
+    */
+
+    public static void main(String[] args) {
+       
+    
+    AccountSignIn signin = new AccountSignIn();
+    signin.setVisible(true);
+}
+}
